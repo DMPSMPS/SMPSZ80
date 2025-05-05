bff606fd17abf8838a97d5afb5523a5ef2f938fd
DMPSMPS <dankscope2091@gmail.com>
2025-05-05 18:04:30 -0400

    Update Main Menu.asm

--- Main Menu.asm
+++ Main Menu.asm
@@ -10,7 +10,7 @@ Snd_MainMenu_Header:
 	smpsHeaderFM        Snd_MainMenu_FM3,	$FF, $20
 	smpsHeaderFM        Snd_MainMenu_FM4,	$FF, $14
 	smpsHeaderFM        Snd_MainMenu_FM5,	$FF, $14
-	smpsHeaderPSG       Snd_MainMenu_PSG1,	$0B, $03, $00, sTone_11
+	smpsHeaderPSG       Snd_MainMenu_PSG1,	$0B, $02, $00, sTone_11
 	smpsHeaderPSG       Snd_MainMenu_PSG2,	$0B, $00, $00, sTone_11
 	smpsHeaderPSG       Snd_MainMenu_PSG3,	$0C, $00, $00, sTone_0F
 
