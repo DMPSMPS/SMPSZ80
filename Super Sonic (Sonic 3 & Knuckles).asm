33ce5e5506b15ee6c71e2144a5e5a653b7acf760
DMPSMPS <dankscope2091@gmail.com>
2025-05-05 12:16:33 -0400

    Update Super Sonic (Sonic 3 & Knuckles).asm

--- Super Sonic (Sonic 3 & Knuckles).asm
+++ Super Sonic (Sonic 3 & Knuckles).asm
@@ -7,7 +7,7 @@ Snd_Supersonic_Header:
 	smpsHeaderDAC       Snd_Supersonic_DAC
 	smpsHeaderFM        Snd_Supersonic_FM1,	$00, $04
 	smpsHeaderFM        Snd_Supersonic_FM2,	$00, $0A
-	smpsHeaderFM        Snd_Supersonic_FM3,	$00, $19
+	smpsHeaderFM        Snd_Supersonic_FM3,	$00, $17
 	smpsHeaderFM        Snd_Supersonic_FM4,	$00, $13
 	smpsHeaderFM        Snd_Supersonic_FM5,	$00, $13
 	smpsHeaderPSG       Snd_Supersonic_PSG1,	$0C, $00, $00, sTone_11
