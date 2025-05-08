3253bc9a24b00845ce2c05ee3a17cfb9e05346f0
DMPSMPS <dankscope2091@gmail.com>
2025-05-08 15:10:19 -0400

    Update SHZ1.asm

--- SHZ1.asm
+++ SHZ1.asm
@@ -10,8 +10,8 @@ Snd_SHZ1_Header:
 	smpsHeaderFM        Snd_SHZ1_FM3,	$00, $1E
 	smpsHeaderFM        Snd_SHZ1_FM4,	$00, $12
 	smpsHeaderFM        Snd_SHZ1_FM5,	$00, $1D
-	smpsHeaderPSG       Snd_SHZ1_PSG1,	$0C, $00, $00, sTone_03
-	smpsHeaderPSG       Snd_SHZ1_PSG2,	$0C, $05, $00, sTone_03
+	smpsHeaderPSG       Snd_SHZ1_PSG1,	$0C, $00, $00, sTone_0A
+	smpsHeaderPSG       Snd_SHZ1_PSG2,	$0C, $05, $00, sTone_17
 	smpsHeaderPSG       Snd_SHZ1_PSG3,	$0C, $00, $00, sTone_0F
 
 ; DAC Data
