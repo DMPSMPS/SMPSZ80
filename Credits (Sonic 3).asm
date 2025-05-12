5430d0658a6e32c1c0a3158a86112a4acf517ef4
DMP <dankscope2091@gmail.com>
2025-05-12 16:49:56 -0400

    Update Credits (Sonic 3).asm

--- Credits (Sonic 3).asm
+++ Credits (Sonic 3).asm
@@ -1,22 +1,22 @@
-s3p38_Header:
+Snd_S3Credits_Header:
 	smpsHeaderStartSong 3
-	smpsHeaderVoice     s3p38_Voices
+	smpsHeaderVoice     Snd_S3Credits_Voices
 	smpsHeaderChan      $06, $03
 	smpsHeaderTempo     $01, $30
 
-	smpsHeaderDAC       s3p38_DAC
-	smpsHeaderFM        s3p38_FM1,	$00, $0E
-	smpsHeaderFM        s3p38_FM2,	$0C, $03
-	smpsHeaderFM        s3p38_FM3,	$00, $0D
-	smpsHeaderFM        s3p38_FM4,	$0C, $1B
-	smpsHeaderFM        s3p38_FM5,	$00, $14
-	smpsHeaderPSG       s3p38_PSG1,	$F4, $03, $00, $00
-	smpsHeaderPSG       s3p38_PSG2,	$F4, $03, $00, $00
-	smpsHeaderPSG       s3p38_PSG3,	$23, $02, $00, $00
+	smpsHeaderDAC       Snd_S3Credits_DAC
+	smpsHeaderFM        Snd_S3Credits_FM1,	$00, $0E
+	smpsHeaderFM        Snd_S3Credits_FM2,	$0C, $03
+	smpsHeaderFM        Snd_S3Credits_FM3,	$00, $0D
+	smpsHeaderFM        Snd_S3Credits_FM4,	$0C, $1B
+	smpsHeaderFM        Snd_S3Credits_FM5,	$00, $14
+	smpsHeaderPSG       Snd_S3Credits_PSG1,	$F4, $03, $00, $00
+	smpsHeaderPSG       Snd_S3Credits_PSG2,	$F4, $03, $00, $00
+	smpsHeaderPSG       Snd_S3Credits_PSG3,	$23, $02, $00, $00
 
 ; FM1 Data
-s3p38_FM1:
-s3p38_Jump01:
+Snd_S3Credits_FM1:
+Snd_S3Credits_Jump01:
 	smpsModSet          $01, $01, $01, $04
 	smpsSetvoice        $00
 	dc.b	nRst, $24, nAb4, $06, nG4, nAb4, $0C, nC4, $06, nC4, nG4, nAb4
@@ -42,38 +42,38 @@ s3p38_Jump01:
 	dc.b	nRst, $06, nBb4, $2A, nRst, $06
 	smpsChangeTransposition $F4
 
-s3p38_Loop04:
+Snd_S3Credits_Loop04:
 	smpsSetvoice        $06
 	dc.b	nRst, $0C, nCs5, $06, nCs5, nRst, nCs5, $0C, nCs5, $06, nCs5, nC5
 	dc.b	nC5, nRst, $2A, nCs5, $06, nCs5, nRst, nCs5, $0C, nCs5, $06, nCs5
 	dc.b	nFs4, nAb4, nC5, nEb5, nRst, $12, nRst, $0C, nCs5, $06, nCs5, nRst
 	dc.b	nCs5, $0C, nCs5, $06, nCs5, nC5, nC5, nRst, $12, nEb5, $06, nCs5
 	dc.b	nRst, nCs5, $12, nC5, $06, nRst, $42
-	smpsLoop            $00, $02, s3p38_Loop04
-	smpsJump            s3p38_Jump01
+	smpsLoop            $00, $02, Snd_S3Credits_Loop04
+	smpsJump            Snd_S3Credits_Jump01
 
 ; FM2 Data
-s3p38_FM2:
-s3p38_Loop01:
+Snd_S3Credits_FM2:
+Snd_S3Credits_Loop01:
 	smpsSetvoice        $04
 	dc.b	nAb1, $06, nRst, nAb1, nRst, $4E
-	smpsLoop            $00, $09, s3p38_Loop01
+	smpsLoop            $00, $09, Snd_S3Credits_Loop01
 
-s3p38_Loop02:
+Snd_S3Credits_Loop02:
 	dc.b	nAb1, $06, nRst, nAb1, nRst, nAb1, $06, nF1, nEb1, nAb1, nRst, $30
 	dc.b	nAb1, $06, nRst, $12, nAb1, $06, nRst, $42, nEb2, $06, nRst, nEb2
 	dc.b	nRst, nEb2, nC2, nBb1, nEb2, nRst, $18, nC2, $0C, nCs2, $06, nC2
 	dc.b	nAb1, nRst, nAb1, nRst, nAb1, nF1, nEb1, nAb1, nRst, nC1, $12, nCs1
 	dc.b	$06, nRst, nEb1, nRst
-	smpsLoop            $00, $02, s3p38_Loop02
+	smpsLoop            $00, $02, Snd_S3Credits_Loop02
 
-s3p38_Loop03:
+Snd_S3Credits_Loop03:
 	dc.b	nFs2, $06, nFs2, nF2, nEb2, nCs2, nC2, nFs1, nRst, $1E, nFs3, $06
 	dc.b	nRst, $1E, nFs2, $06, nFs2, nF2, nEb2, nCs2, nC2, nAb1, nFs1, nFs2
 	dc.b	nRst, $1E, nF2, $06, nF2, nC3, nF2, nAb2, nC3, nF3, nRst, $1E
 	dc.b	nF2, $06, nRst, $1E, nBb2, $06, nBb2, nAb2, nG2, nF2, nEb2, nD2
 	dc.b	nBb1, nBb2, nRst, $1E
-	smpsLoop            $00, $02, s3p38_Loop03
+	smpsLoop            $00, $02, Snd_S3Credits_Loop03
 	dc.b	nRst, $0C, nFs1, $06, nFs1, nRst, nFs1, $0C, nFs1, $06, nFs1, nFs1
 	dc.b	nFs1, nRst, $2A, nFs1, $06, nFs1, nRst, nFs1, $0C, nFs1, $06, nFs1
 	dc.b	nRst, $12, nFs1, $06, nRst, nC2, nEb2, nRst, $0C, nFs1, $06, nFs1
@@ -85,10 +85,10 @@ s3p38_Loop03:
 	dc.b	$06, nFs1, nRst, nFs1, $0C, nFs1, $06, nFs1, nFs1, nFs1, nRst, $12
 	dc.b	nAb1, $06, nAb1, nRst, nAb1, nAb1, nRst, nAb1, nRst, $2A, nAb1, $06
 	dc.b	nEb1, nC2, $0C
-	smpsJump            s3p38_Loop01
+	smpsJump            Snd_S3Credits_Loop01
 
 ; FM3 Data
-s3p38_FM3:
+Snd_S3Credits_FM3:
 	smpsModSet          $07, $01, $03, $05
 	smpsPan             panRight, $00
 	smpsSetvoice        $02
@@ -150,10 +150,10 @@ s3p38_FM3:
 	dc.b	nAb3, nAb3, nC4, nC4, nEb4, nEb4, nAb3, nAb3
 	smpsChangeTransposition $F4
 	smpsFMAlterVol      $F3
-	smpsJump            s3p38_FM3
+	smpsJump            Snd_S3Credits_FM3
 
 ; FM4 Data
-s3p38_FM4:
+Snd_S3Credits_FM4:
 	smpsFMAlterVol	    $04
 	smpsModSet          $01, $01, $01, $07
 	smpsPan             panLeft, $00
@@ -196,30 +196,30 @@ s3p38_FM4:
 	dc.b	nC5, nEb4, nEb4, nAb4, nAb4, nC5, nC5, nEb4, nEb4, nAb4, nAb4, nC5
 	dc.b	nC5, nEb4, nEb4, nAb4, nAb4
 	smpsFMAlterVol      $FE
-	smpsJump            s3p38_FM4
+	smpsJump            Snd_S3Credits_FM4
 
 ; FM5 Data
-s3p38_FM5:
+Snd_S3Credits_FM5:
 	smpsModSet          $01, $01, $01, $08
 	dc.b	nRst, $0B
 	smpsAlterNote          $01
-	smpsJump            s3p38_Jump01
+	smpsJump            Snd_S3Credits_Jump01
 
 ; PSG1 Data
-s3p38_PSG1:
+Snd_S3Credits_PSG1:
 	smpsModSet          $02, $01, $01, $05
 	smpsPSGvoice        sTone_11
 	dc.b	nC4, $60, nD4, $60, nEb4, $60, nF4, $60, nC4, $60, nD4, $60
 	dc.b	nEb4, $30, nF4, nC4, $60, smpsNoAttack, $60
 
-s3p38_Loop06:
+Snd_S3Credits_Loop06:
 	smpsPSGvoice        sTone_1D
 	smpsPSGAlterVol     $FD
 	dc.b	nC4, $06, nRst, nC4, nRst, $0C, nC4, nC4, $36, nD4, $06, nRst
 	dc.b	$12, nD4, $48, nEb4, $06, nRst, nEb4, nRst, $0C, nEb4, nEb4, $36
 	dc.b	nC4, $60
 	smpsPSGAlterVol     $03
-	smpsLoop            $00, $02, s3p38_Loop06
+	smpsLoop            $00, $02, Snd_S3Credits_Loop06
 	smpsPSGvoice        sTone_1D
 	smpsPSGAlterVol     $FE
 	dc.b	nCs4, $30, nFs4, nEb4, nC5, nC4, nAb4, nEb4, $18, nD4, nBb4, nBb3
@@ -237,23 +237,23 @@ s3p38_Loop06:
 	dc.b	nC4, nRst, $12, nCs4, $06, nCs4, nRst, nCs4, $12, nEb4, $06, nRst
 	dc.b	$42
 	smpsPSGAlterVol     $02
-	smpsJump            s3p38_PSG1
+	smpsJump            Snd_S3Credits_PSG1
 
 ; PSG2 Data
-s3p38_PSG2:
+Snd_S3Credits_PSG2:
 	smpsModSet          $02, $01, $01, $05
 	smpsPSGvoice        sTone_11
 	dc.b	nAb3, $60, nBb3, nB3, nCs4, nAb3, nBb3, nB3, $30, nCs4, nAb3, $60
 	dc.b	smpsNoAttack, $60
 
-s3p38_Loop05:
+Snd_S3Credits_Loop05:
 	smpsPSGvoice        sTone_1D
 	smpsPSGAlterVol     $FD
 	dc.b	nAb3, $06, nRst, nAb3, nRst, $0C, nAb3, nAb3, $36, nBb3, $06, nRst
 	dc.b	$12, nBb3, $48, nBb3, $06, nRst, nBb3, nRst, $0C, nBb3, nBb3, $36
 	dc.b	nAb3, $60
 	smpsPSGAlterVol     $03
-	smpsLoop            $00, $02, s3p38_Loop05
+	smpsLoop            $00, $02, Snd_S3Credits_Loop05
 	smpsPSGAlterVol     $FE
 	smpsPSGvoice        sTone_1D
 	dc.b	nFs3, $30, nBb3, nAb3, nEb4, nAb3, nC4, nF3, $18, nF3, nD4, nF3
@@ -271,13 +271,13 @@ s3p38_Loop05:
 	dc.b	nAb3, nAb3, nRst, $12, nBb3, $06, nBb3, nRst, nBb3, $12, nC4, $06
 	dc.b	nRst, $42
 	smpsPSGAlterVol     $02
-	smpsJump            s3p38_PSG2
+	smpsJump            Snd_S3Credits_PSG2
 
 ; PSG3 Data
-s3p38_PSG3:
+Snd_S3Credits_PSG3:
 	smpsPSGform         $E7
-	smpsCall            s3p38_Call00
-	smpsCall            s3p38_Call01
+	smpsCall            Snd_S3Credits_Call00
+	smpsCall            Snd_S3Credits_Call01
 	smpsPSGvoice        sTone_0F
 	dc.b	(nMaxPSG2-$23)&$FF, $06, (nMaxPSG2-$23)&$FF
 	smpsPSGvoice        sTone_12
@@ -286,9 +286,9 @@ s3p38_PSG3:
 	dc.b	(nMaxPSG2-$23)&$FF, $06
 	smpsPSGvoice        sTone_12
 	dc.b	(nMaxPSG2-$23)&$FF, $0C, (nMaxPSG2-$23)&$FF, $36
-	smpsCall            s3p38_Call02
-	smpsCall            s3p38_Call02
-	smpsCall            s3p38_Call03
+	smpsCall            Snd_S3Credits_Call02
+	smpsCall            Snd_S3Credits_Call02
+	smpsCall            Snd_S3Credits_Call03
 	smpsPSGvoice        sTone_0F
 	dc.b	(nMaxPSG2-$23)&$FF, $06, (nMaxPSG2-$23)&$FF
 	smpsPSGvoice        sTone_12
@@ -299,29 +299,29 @@ s3p38_PSG3:
 	dc.b	$0C
 	smpsPSGvoice        sTone_12
 	dc.b	(nMaxPSG2-$23)&$FF, $18, $18
-	smpsCall            s3p38_Call00
-	smpsCall            s3p38_Call01
-	smpsJump            s3p38_PSG3
+	smpsCall            Snd_S3Credits_Call00
+	smpsCall            Snd_S3Credits_Call01
+	smpsJump            Snd_S3Credits_PSG3
 
-s3p38_Call00:
+Snd_S3Credits_Call00:
 	smpsPSGvoice        sTone_0F
 	dc.b	(nMaxPSG2-$23)&$FF, $06, (nMaxPSG2-$23)&$FF
 	smpsPSGvoice        sTone_12
 	dc.b	$0C
-	smpsLoop            $00, $10, s3p38_Call00
+	smpsLoop            $00, $10, Snd_S3Credits_Call00
 	smpsReturn
 
-s3p38_Call01:
+Snd_S3Credits_Call01:
 	smpsPSGvoice        sTone_0F
 	dc.b	(nMaxPSG2-$23)&$FF, $06, (nMaxPSG2-$23)&$FF
 	smpsPSGvoice        sTone_12
 	dc.b	(nMaxPSG2-$23)&$FF
 	smpsPSGvoice        sTone_0F
 	dc.b	(nMaxPSG2-$23)&$FF
-	smpsLoop            $00, $10, s3p38_Call01
+	smpsLoop            $00, $10, Snd_S3Credits_Call01
 	smpsReturn
 
-s3p38_Call02:
+Snd_S3Credits_Call02:
 	smpsPSGvoice        sTone_0F
 	dc.b	(nMaxPSG2-$23)&$FF, $06, nRst, (nMaxPSG2-$23)&$FF, (nMaxPSG2-$23)&$FF
 	smpsPSGvoice        sTone_12
@@ -334,10 +334,10 @@ s3p38_Call02:
 	dc.b	(nMaxPSG2-$23)&$FF, $0C
 	smpsPSGvoice        sTone_0F
 	dc.b	$06, $06
-	smpsLoop            $00, $04, s3p38_Call02
+	smpsLoop            $00, $04, Snd_S3Credits_Call02
 	smpsReturn
 
-s3p38_Call03:
+Snd_S3Credits_Call03:
 	smpsPSGvoice        sTone_0F
 	dc.b	(nMaxPSG2-$23)&$FF, $06, (nMaxPSG2-$23)&$FF, (nMaxPSG2-$23)&$FF, (nMaxPSG2-$23)&$FF
 	smpsPSGvoice        sTone_12
@@ -348,23 +348,23 @@ s3p38_Call03:
 	dc.b	(nMaxPSG2-$23)&$FF, $06
 	smpsPSGvoice        sTone_0F
 	dc.b	(nMaxPSG2-$23)&$FF, (nMaxPSG2-$23)&$FF, (nMaxPSG2-$23)&$FF, (nMaxPSG2-$23)&$FF, (nMaxPSG2-$23)&$FF, (nMaxPSG2-$23)&$FF, (nMaxPSG2-$23)&$FF
-	smpsLoop            $00, $07, s3p38_Call03
+	smpsLoop            $00, $07, Snd_S3Credits_Call03
 	smpsReturn
 
 ; DAC Data
-s3p38_DAC:
+Snd_S3Credits_DAC:
 	dc.b	dCrashCymbal, $60, dCrashCymbal, $48, dElectricMidTom, $02, dElectricFloorTom, $16, dCrashCymbal, $48, dSnareS3, $03
 	dc.b	dSnareS3, $15, dCrashCymbal, $18, dElectricHighTom, dKickS3, dElectricFloorTom, dCrashCymbal, $18, dKickS3, nRst, dKickS3
 	dc.b	nRst, dKickS3, nRst, dKickS3, dKickS3, $18, dKickS3, dKickS3, dKickS3, dKickS3, dKickS3, dKickS3
 	dc.b	dKickS3, dKickS3, $0C, dSnareS3, $06, nRst, dSnareS3, dSnareS3, nRst, dSnareS3, nRst, dHighTom
 	dc.b	dHighTom, dHighTom, dMidTomS3, dMidTomS3, dLowTomS3, dLowTomS3
 
-s3p38_Loop00:
+Snd_S3Credits_Loop00:
 	dc.b	dCrashCymbal, $06, nRst, dKickS3, nRst, dSnareS3, dKickS3, nRst, dKickS3, $1E, dSnareS3, $18
 	dc.b	dKickS3, $18, dSnareS3, $12, dKickS3, $06, dKickS3, nRst, dKickS3, nRst, dSnareS3, $18
 	dc.b	dKickS3, $06, nRst, dKickS3, nRst, dSnareS3, dKickS3, nRst, dKickS3, $1E, dSnareS3, $18
 	dc.b	dKickS3, $18, dSnareS3, dKickS3, dSnareS3
-	smpsLoop            $00, $02, s3p38_Loop00
+	smpsLoop            $00, $02, Snd_S3Credits_Loop00
 	dc.b	dKickS3, $18, dSnareS3, $12, dKickS3, $06, dKickS3, $18, dCrashCymbal, dKickS3, dSnareS3, $12
 	dc.b	dKickS3, $06, dKickS3, $18, dSnareS3, $03, dSnareS3, $15, dKickS3, $18, dSnareS3, $12
 	dc.b	dKickS3, $06, dKickS3, $18, dSnareS3, dKickS3, $18, dSnareS3, dKickS3, dSnareS3, dKickS3, $18
@@ -379,7 +379,7 @@ s3p38_Loop00:
 	dc.b	nRst, dSnareS3, nRst, nRst, dCrashCymbal, dHighTom, dHighTom, dHighTom, dMidTomS3, dMidTomS3, dMidTomS3, dMidTomS3
 	dc.b	dLowTomS3, dLowTomS3, dFloorTomS3, dFloorTomS3
 
-s3p38_Jump00:
+Snd_S3Credits_Jump00:
 	dc.b	dCrashCymbal, $06, nRst, dKickS3, nRst, dSnareS3, dKickS3, nRst, dKickS3, $1E, dSnareS3, $18
 	dc.b	dKickS3, $18, dSnareS3, $12, dKickS3, $06, dKickS3, nRst, dKickS3, nRst, dSnareS3, $18
 	dc.b	dKickS3, $06, nRst, dKickS3, nRst, dSnareS3, dKickS3, nRst, dKickS3, $1E, dSnareS3, $18
@@ -406,9 +406,9 @@ s3p38_Jump00:
 	dc.b	dSnareS3, dSnareS3, dSnareS3, dSnareS3, nRst, nRst, nRst, dSnareS3, dSnareS3, nRst, dSnareS3, nRst
 	dc.b	nRst, dCrashCymbal, dHighTom, dHighTom, dHighTom, dMidTomS3, dMidTomS3, dMidTomS3, dMidTomS3, dLowTomS3, dLowTomS3, dFloorTomS3
 	dc.b	dFloorTomS3
-	smpsJump            s3p38_Jump00
+	smpsJump            Snd_S3Credits_Jump00
 
-s3p38_Voices:
+Snd_S3Credits_Voices:
 ;	Voice $00
 ;	$3A
 ;	$3F, $7D, $73, $31, 	$9F, $5F, $54, $17, 	$0D, $14, $0A, $05
